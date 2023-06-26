75493966a7746edbf6e6cdb571a3d52230723bd3
mehedihasanreal <mehedihasan6002@gmail.com>
2023-06-26 21:20:38 +0600

    email intrigated to Bngmultiservice

--- script.js
+++ script.js
@@ -3,38 +3,38 @@ const nav = document.getElementById('navbar');
 const close = document.getElementById("close");
 
 if (bar) {
-    bar.addEventListener("click", () => {
-        nav.classList.add("active");
-    });
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
 }
 
 if (close) {
-    close.addEventListener("click", () => {
-        nav.classList.remove("active");
-    });
+  close.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 }
 
 function sendMail() {
-    let params = {
-      name: document.getElementById("name").value,
-      email: document.getElementById("email").value,
-      subject: document.getElementById("subject").value,
-      message: document.getElementById("message").value,
-    };
-  
-    const serviceID = "service_95pdrgj";
-    const templateID = "template_h9ed7jq";
-  
-    emailjs
-      .send(serviceID, templateID, params)
-      .then((res) => {
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("subject").value= "",
+  let params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_kosx2hu";
+  const templateID = "template_7molvyt";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "",
         document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!");
-      })
-      .catch((error) => console.log(error));
-    console.log(params);
+      console.log(res);
+      alert("Your message sent successfully!!");
+    })
+    .catch((error) => console.log(error));
+  console.log(params);
 }
